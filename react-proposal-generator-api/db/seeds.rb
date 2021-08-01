5f7eb0f56bb3790a8f548fb31a3ee33b49c7844b
Sam Bequette <sbb3391@gmail.com>
2021-07-31 21:52:34 -0400

    added models to db

--- react-proposal-generator-api/db/seeds.rb
+++ react-proposal-generator-api/db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Model.create(
+  part_number: "ACW3011",
+  name: "accuriopress C7090",
+  branch_floor_price: "47570.42",
+  srp: "51,961.54"
+)
+
+Model.create(
+  part_number: "AC0C011",
+  name: "accuriopress C14000",
+  branch_floor_price: "113544.23",
+  srp: "124025.23"
+)
