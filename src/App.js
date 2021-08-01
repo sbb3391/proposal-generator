de321107fa629bc084063852f6cf87d1a4c1d98a
Sam Bequette <sbb3391@gmail.com>
2021-07-31 20:55:18 -0400

    adding rails api to repo

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Home from './containers/Home.js';
+import Navbar from './containers/Navbar'
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
