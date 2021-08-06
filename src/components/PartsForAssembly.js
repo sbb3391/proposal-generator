9fa568fb74989ea8577901504ef4b23c630b9250
Sam Bequette <sbb3391@gmail.com>
2021-08-06 09:45:01 -0400

    added ternary to not show required options when choosing assembly items

--- src/components/PartsForAssembly.js
+++ src/components/PartsForAssembly.js
@@ -18,19 +18,26 @@ class PartsForAssembly extends Component {
     })
   }
 
-  renderCheckbox = (itemAssembly) => {
+  renderItemDetails = (itemAssembly) => {
    return itemAssembly.required ? 
-    <input className="h-8 pl-8" type="checkbox" checked disabled /> :
-    <input className="h-8 pl-8" type="checkbox" />
+    // <input className="h-8 pl-8" type="checkbox" checked disabled /> 
+    null :
+    <div className="flex space-x-5">
+      <input className="h-8 pl-8" type="checkbox" />
+      <label className="text-xl">{itemAssembly.item.description}</label>
+      <label className="text-sm">({itemAssembly.item.itemNumber})</label>
+    </div>
+  }
+
+  renderLabels = () => {
+
   }
 
   renderItems = () => {
     return this.state.itemsAssemblies.map( itemAssembly => {
       return(
         <div className="flex space-x-5">
-          {this.renderCheckbox(itemAssembly)}
-          <label className="text-xl">{itemAssembly.item.description}</label>
-          <label className="text-sm">({itemAssembly.item.itemNumber})</label>
+          {this.renderItemDetails(itemAssembly)}
         </div>
       )
     })
@@ -44,7 +51,7 @@ class PartsForAssembly extends Component {
     return (
       <div className="relative z-20 w-full h-full bg-red-100 flex place-items-center">
         <div className="w-2/3 h-5/6 mx-auto flex flex-col">
-          <h1 className="w-2/3 mx-auto text-center">Select Parts for {this.state.assemblyName}:</h1>
+          <h1 className="w-2/3 mx-auto text-center">Select Options For {this.state.assemblyName}:</h1>
           <div>
             <form>
               {this.renderItems()}
